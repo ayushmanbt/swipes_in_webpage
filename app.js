814c4cdf9290df1a4365b9db63a9247131ce95e3
Ayushman Bilas Thakur <ayushmanbilasthakur@gmail.com>
2020-04-11 00:13:45 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const card = document.querySelector('.card');
+const theDeleteButton = document.querySelector('.delete_button');
+
+const MOVE_THRESHOLD = 100
+
+let initialX = 0;
+let moveX = 0;
+let isDeleteButtonOpen = false;
+
+
+card.addEventListener('touchstart', (e) => {
+    initialX = e.touches[0].pageX;
+})
+
+card.addEventListener('touchmove', (e) => {
+    let currentX = e.touches[0].pageX;
+    moveX = currentX - initialX;
+})
+
+card.addEventListener('touchend', (e) => {
+    if(moveX < MOVE_THRESHOLD && !isDeleteButtonOpen){
+        theDeleteButton.style.transform = "scaleX(1)";
+        isDeleteButtonOpen = !isDeleteButtonOpen;
+    }
+    else if(moveX > MOVE_THRESHOLD && isDeleteButtonOpen){
+        theDeleteButton.style.transform = "scaleX(0)";
+        isDeleteButtonOpen = !isDeleteButtonOpen;
+    }
+})
