58b7b49a65063d407237b7d9ae0f3e3f1996d4d6
Ayushman Bilas Thakur <ayushmanbilasthakur@gmail.com>
2020-04-13 03:36:21 +0530

    some changes

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 const card = document.querySelector(".card");
 const theDeleteButton = document.querySelector(".delete_button");
 
-const MOVE_THRESHOLD = 100;
+const MOVE_THRESHOLD = 50;
 
 let initialX = 0;
 let moveX = 0;
 let isDeleteButtonOpen = false;
 
 card.addEventListener("touchstart", e => {
-  initialX = e.touches[0].pageX;
+    initialX = e.touches[0].pageX;
 });
 
 card.addEventListener("touchmove", e => {
+  
   let currentX = e.touches[0].pageX;
   moveX = currentX - initialX;
 });
 
 card.addEventListener("touchend", e => {
-  if (moveX < MOVE_THRESHOLD && !isDeleteButtonOpen) {
+  if (moveX < MOVE_THRESHOLD * Math.sign(moveX) && !isDeleteButtonOpen) {
     theDeleteButton.style.transform = "scaleX(1)";
     isDeleteButtonOpen = !isDeleteButtonOpen;
-  } else if (moveX > MOVE_THRESHOLD && isDeleteButtonOpen) {
+  } else if (moveX > MOVE_THRESHOLD * Math.sign(moveX) && isDeleteButtonOpen) {
     theDeleteButton.style.transform = "scaleX(0)";
     isDeleteButtonOpen = !isDeleteButtonOpen;
   }
